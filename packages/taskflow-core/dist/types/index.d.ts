a67ef82893c1526a20edc615152f4336ffd57d49
Hoang Hong Huy <huyhoangok2212@gmail.com>
2025-11-19 02:57:15 +0700

    chore: remove legacy taskflow-core package

--- packages/taskflow-core/dist/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './common';
-export * from './task';
-export * from './habit';
-export * from './pomodoro';
-export * from './countdown';
-export * from './app';
-export * from './mobile';
